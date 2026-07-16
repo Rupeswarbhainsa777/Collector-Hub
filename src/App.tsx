87804316681ee42bb873d0a9075c1775ce74a874
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-07-17 00:18:28 +0530

    all requir pages are add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
 import './App.css'
+import {Route, Routes} from "react-router";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
 
 
-  return (
-    <>H</>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Navbar/>}/>
+
+        </Routes>
+    )
 }
 
-export default App
+export default App;
