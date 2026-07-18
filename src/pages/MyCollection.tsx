3373312b94fab8ff558dd86d0c16c504c090e590
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-07-19 04:02:22 +0530

    Start working on collection

--- src/pages/MyCollection.tsx
+++ src/pages/MyCollection.tsx
@@ -1,5 +1,18 @@
+import type {Category, CollectionName} from "../types";
 
 
+const TABS: CollectionName[] = ['Owned', 'Wishlist', 'Selling'];
+
+const CATEGORIES: Category[] = [
+    'Coins',
+    'Stamps',
+    'Trading Cards',
+    'Comics',
+    'Vintage Toys',
+    'Watches',
+    'Vinyl Records',
+    'Sports Memorabilia',
+];
 const MyCollection = () =>{
     return(
         <h2>My Collection</h2>
